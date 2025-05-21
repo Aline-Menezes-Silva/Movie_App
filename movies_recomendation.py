34db166867f4528f54e4746be5d23ae8fe5c7099
Aline-Menezes-Silva <sba23164@student.cct.ie>
2025-05-21 09:43:06 +0100

    first step

--- movies_recomendation.py
+++ movies_recomendation.py
@@ -10,3 +10,21 @@ from io import BytesIO
 import base64
 
 st.title ("Movies Analysis")
+
+# Load datasets
+
+
+encodings_to_try = ['utf-8', 'latin1', 'iso-8859-1', 'cp1252']
+
+for encoding in encodings_to_try:
+    try:
+        movies = pd.read_csv("movies.csv", encoding=encoding)
+        ratings = pd.read_csv('rating.csv', encoding=encoding)
+        tags = pd.read_csv('tags.csv', encoding=encoding)
+        print(f"Successfully loaded with {encoding} encoding")
+        break
+    except UnicodeDecodeError:
+        continue
+else:
+    raise ValueError("None of the encodings worked for your files") 
+
