0256d868d21a6f57b4dd515dfd139704d0e1dc83
Aline-Menezes-Silva <sba23164@student.cct.ie>
2025-05-21 10:21:31 +0100

    Update movies_recomendation.py

--- movies_recomendation.py
+++ movies_recomendation.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 import pandas as pd
-import plotly.express as px
+
 import plotly.graph_objects as go
 from dash import Dash, dcc, html, Input, Output
 import dash_bootstrap_components as dbc
