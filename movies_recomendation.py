d552155da4778fa6b023b37a377c3490bf565e68
Aline-Menezes-Silva <sba23164@student.cct.ie>
2025-05-21 11:55:42 +0100

    c

--- movies_recomendation.py
+++ movies_recomendation.py
@@ -1,8 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.express as px
-import io
+
 
 # Set page config
 st.set_page_config(layout="wide", page_title="Movie Analysis Dashboard")
