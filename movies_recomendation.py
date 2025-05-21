c617d9d8ba9a18d467dfe48d01ae0bd1422428cd
Aline-Menezes-Silva <sba23164@student.cct.ie>
2025-05-21 10:05:01 +0100

    Update movies_recomendation.py

--- movies_recomendation.py
+++ movies_recomendation.py
@@ -1,7 +1,14 @@
 import streamlit as st
 
 import pandas as pd
-
+import plotly.express as px
+import plotly.graph_objects as go
+from dash import Dash, dcc, html, Input, Output
+import dash_bootstrap_components as dbc
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+from io import BytesIO
+import base64
 
 st.title ("Movies Analysis")
 
